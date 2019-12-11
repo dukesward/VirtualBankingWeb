387734b5c6d1a1ac497a6c4e27879cfdcb24b403
Troy <398798254@qq.com>
2019-12-11 19:13:09 +0800

    enhance 1

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,28 +1,36 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard.component';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { DashboardRoutingModule } from './dashboard-routing.module';
-import { AccountSummaryComponent } from '../account-summary/account-summary.component';
-import { FunddetailComponent } from '../funddetail/funddetail.component';
-import { BuyComponentComponent } from '../buy-component/buy-component.component';
-import { PrelophomeComponent } from '../prelophome/prelophome.component';
-import { PrelopinputComponent } from '../prelopinput/prelopinput.component';
-import { PrelopconfirmComponent } from '../prelopconfirm/prelopconfirm.component';
-import { PrelopsuccessComponent } from '../prelopsuccess/prelopsuccess.component';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DashboardComponent } from "./dashboard.component";
+import { NzMenuModule } from "ng-zorro-antd/menu";
+import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { AccountSummaryComponent } from "../account-summary/account-summary.component";
+import { FunddetailComponent } from "../funddetail/funddetail.component";
+import { BuyComponentComponent } from "../buy-component/buy-component.component";
+import { PrelophomeComponent } from "../prelophome/prelophome.component";
+import { PrelopinputComponent } from "../prelopinput/prelopinput.component";
+import { PrelopconfirmComponent } from "../prelopconfirm/prelopconfirm.component";
+import { PrelopsuccessComponent } from "../prelopsuccess/prelopsuccess.component";
+import { NzCollapseModule } from "ng-zorro-antd/collapse";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { NzButtonModule } from "ng-zorro-antd";
 @NgModule({
-  declarations: [DashboardComponent,AccountSummaryComponent,
+  declarations: [
+    DashboardComponent,
+    AccountSummaryComponent,
     FunddetailComponent,
     BuyComponentComponent,
     PrelophomeComponent,
     PrelopinputComponent,
     PrelopconfirmComponent,
-    PrelopsuccessComponent],
+    PrelopsuccessComponent
+  ],
   imports: [
     CommonModule,
     NzMenuModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    NzCollapseModule,
+    NzIconModule,
+    NzButtonModule
   ]
 })
-export class DashboardModule { }
+export class DashboardModule {}
